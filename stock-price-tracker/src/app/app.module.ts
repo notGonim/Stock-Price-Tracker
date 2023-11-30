83105498d92118cec6c606138db6a9cafdfaaf1b
gonim <69145711+notGonim@users.noreply.github.com>
2023-11-30 11:57:15 +0200

    init:INITIALIZE THE REPO && ADDED THE DOCKER FILES

---
+++ stock-price-tracker/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, MatButtonModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
